bf76fa33577e93346177b693f1a5382e8f6482c8
Hamza <hamza.khalifa.clashingames@gmail.com>
2024-03-16 20:25:30 +0100

    cart _id

--- src/elements/cart/ICartReadDto.ts
+++ src/elements/cart/ICartReadDto.ts
@@ -2,6 +2,7 @@ import { IEntityReadDto } from "../entity/IEntityReadDto";
 import { IUserReadDto } from "../user/IUserReadDto";
 
 export interface ICartReadDto {
+  _id: string;
   user: IUserReadDto | string;
   products: {
     quantity: number;
